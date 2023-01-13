fdcd15857903f6f8982fd198e0b3e381e0666128
Irena Demchenkova <iren.demchenkova@gmail.com>
2023-01-14 00:38:58 +0300

    Task 2. Frequancy of operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
         final int numberOfPools = 1000;
         final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+        int counter = 0;
         Integer maxFrequency = 0;
         Integer reps = 0;
+        Thread thread = null;
 
+        //searching for max frequancy and corresponding key in HashMap
+
+        thread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Current maximum is " + Collections.max(sizeToFreq.values()));
+                }
+            }
+        });
+        thread.start();
         ExecutorService executor = Executors.newFixedThreadPool(numberOfPools);
         for (int i = 0; i < numberOfPools; i++) {
             Future<Integer> future = executor.submit(new Robot());
@@ -20,14 +37,19 @@ public class Main {
             synchronized (sizeToFreq) {
                 if (!sizeToFreq.containsKey(frequency)) {
                     sizeToFreq.put(frequency, 1);
+                    counter++;
                 } else {
                     Integer value = sizeToFreq.get(frequency) + 1;
                     sizeToFreq.put(frequency, value);
+                    counter++;
                 }
+                sizeToFreq.notify();
+            }
+            if (counter == numberOfPools) {
+                thread.interrupt();
             }
         }
         executor.shutdown();
-        //searching for max frequancy and corresponding key in HashMap
         maxFrequency = (Collections.max(sizeToFreq.values()));
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             if (entry.getValue() == maxFrequency) {
